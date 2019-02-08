26598a6d2f0ef552b197b48cc61c365f71a3985b
Edi Wang <Edi.Wang@outlook.com>
2019-02-08 21:33:01 +0800

    update code

--- CogSvcLngDetect/LngDetectConsole/Program.cs
+++ CogSvcLngDetect/LngDetectConsole/Program.cs
@@ -9,23 +9,30 @@ namespace LngDetectConsole
     {
         static void Main(string[] args)
         {
-            var text1 = "Empower every person and every organization on the planet to achieve more";
-            var text2 = "予力地球上每一人、每一组织，成就不凡";
+            var texts = new[]
+            {
+                "Empower every person and every organization on the planet to achieve more",
+                "予力地球上每一人、每一组织，成就不凡"
+            };
 
             var dt = new TextLanguageDetector("3d7be7cd961e4072bc4359e07b23d23d");
-            var result = dt.DetectAsync(text2).Result;
-            if (result.IsSuccess)
+            foreach (var text in texts)
             {
-                var r = result.ToCogResults();
+                var result = dt.DetectAsync(text).Result;
+                if (result.IsSuccess)
+                {
+                    var r = result.ToCogResults();
 
-                var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
-                var ctr = cultures.FirstOrDefault(c => c.Name == r.First().Language);
-                if (ctr != null) Console.WriteLine($"{ctr.EnglishName} - {ctr.NativeName}");
+                    var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+                    var ctr = cultures.FirstOrDefault(c => c.Name == r.First().Language);
+                    if (ctr != null) Console.WriteLine($"{ctr.EnglishName} - {ctr.NativeName}");
+                }
+                else
+                {
+                    Console.WriteLine(result.ErrorMessage);
+                }
             }
-            else
-            {
-                Console.WriteLine(result.ErrorMessage);
-            }
+            
             Console.ReadKey();
         }
     }
